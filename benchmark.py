7f4a6401ebcd49d5585ef16519a4e74b080a653c
kheizaran-nazari-khakeshoori <kheizarannazarikhakeshoori@gmail.com>
2026-01-29 16:40:56 +0100

    plot pareto curve for comm cost vs accuracy

--- benchmark.py
+++ benchmark.py
@@ -1,2 +1,6 @@
 from algorithms import get_algorithm
 print("benchmark fedavg vs fedprox on alpha 0.1/10")
+
+# pareto curve comm vs acc
+import matplotlib.pyplot as plt
+def pareto(xs, ys): plt.plot(xs, ys)
