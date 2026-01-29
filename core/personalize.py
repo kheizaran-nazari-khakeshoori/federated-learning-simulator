b792a95359368f59377f190e4721bbe892a6db32
kheizaran-nazari-khakeshoori <kheizarannazarikhakeshoori@gmail.com>
2026-01-29 16:40:56 +0100

    add pfedme local update with morau envelope

--- core/personalize.py
+++ core/personalize.py
@@ -1,3 +1,7 @@
 def fedper_freeze(model):
     # freeze base features
     for p in model.parameters(): p.requires_grad=False
+
+def pfedme_update(model, lr=0.01, lam=15):
+    # Moreau envelope
+    pass
